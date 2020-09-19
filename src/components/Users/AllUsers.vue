2dde955680df55a2c7a378042e2bc102a8c570b2
Dasol Park <dasol.park615@gmail.com>
2020-09-19 19:03:55 +0900

    mapState & mapGetters

--- src/components/Users/AllUsers.vue
+++ src/components/Users/AllUsers.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <h1>Users ({{ $store.getters.countOfUsers }})</h1>
-    <h3>User of Seoul: {{ $store.getters.userOfSeoul }}</h3>
-    <h3>User percent of Seoul: {{ $store.getters.percentOfSeoul }}%</h3>
+    <h1>Users ({{ count }})</h1>
+    <h3>User of Seoul: {{ seoul }}</h3>
+    <h3>User percent of Seoul: {{ percent }}%</h3>
     <v-list two-line>
-      <v-list-tile v-for="user in $store.state.users" :key="user.userId" avatar>
+      <v-list-tile v-for="user in users" :key="user.userId" avatar>
         <v-list-tile-avatar color="grey lighten-3">
           <img :src="user.src" />
         </v-list-tile-avatar>
@@ -20,10 +20,19 @@
 
 <script>
 import { EventBus } from "@/main.js";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   data() {
     return {};
+  },
+  computed: {
+    ...mapState(["users"]),
+    ...mapGetters({
+      count: "countOfUsers",
+      seoul: "userOfSeoul",
+      percent: "percentOfSeoul",
+    }),
   },
   mounted() {
     EventBus.$on("signUp", (user) => {
